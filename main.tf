ef618d5279f7a2c31961cb8f224b2beddce6958c
Christian Kappen <christian.kappen@cura4you.de>
2023-07-10 17:02:31 +0200

    feat: Initial commit

--- main.tf
+++ main.tf
@@ -1,10 +1,93 @@
 /**
- * # Terraform AWS Module Template
+ * # Terraform AWS CloudFront Web Application Firewall
  *
- * A template repository for creating our AWS Terraform modules. It gives you a good starting point for creating new modules quickly.
- * It comes with:
- * * Basic directory structure
- * * GitHub Workflow for Linting and Validation
- * * Pre-Commit Hooks
- * * Makefile for common tasks
+ * Terraform module to create a CloudFront Web Application Firewall. Please note, since this is a CloudFront WebACL,
+ * it is mandatory to run in the "us-east-1" region. Check the example.
  */
+resource "aws_wafv2_web_acl" "main" {
+  name  = var.name
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+    priority = 0
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = module.cloudwatch_log_group.name
+    sampled_requests_enabled   = true
+  }
+
+  tags = var.tags
+}
+
+module "cloudwatch_log_group" {
+  source  = "geekcell/cloudwatch-log-group/aws"
+  version = ">= 1.0.1, < 2.0.0"
+
+  name = "/aws/cloudfront/waf/${var.name}"
+}
