f6713bdb645070858d678a891bc9883ecd6bd024
Christian Kappen <christian.kappen@cura4you.de>
2023-07-14 17:59:50 +0200

    chose: fix indentation

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -22,22 +22,33 @@ data "aws_iam_policy_document" "main" {
     effect = "Allow"
 
     principals {
-      identifiers = ["delivery.logs.amazonaws.com"]
-      type        = "Service"
+      identifiers = [
+        "delivery.logs.amazonaws.com"
+      ]
+      type = "Service"
     }
 
-    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
-    resources = ["${module.cloudwatch_log_group.arn}:*"]
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${module.cloudwatch_log_group.arn}:*"
+    ]
 
     condition {
-      test     = "ArnLike"
-      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+      test   = "ArnLike"
+      values = [
+        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+      ]
       variable = "aws:SourceArn"
     }
 
     condition {
-      test     = "StringEquals"
-      values   = [tostring(data.aws_caller_identity.current.account_id)]
+      test   = "StringEquals"
+      values = [
+        tostring(data.aws_caller_identity.current.account_id)
+      ]
       variable = "aws:SourceAccount"
     }
   }
