616448bbe301ca635d0e6db23e14d6354ddf6037
Christian Kappen <christian.kappen@cura4you.de>
2023-07-14 18:00:23 +0200

    chose: fix indentation

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "main" {
     ]
 
     condition {
-      test = "ArnLike"
+      test   = "ArnLike"
       values = [
         "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
       ]
@@ -45,7 +45,7 @@ data "aws_iam_policy_document" "main" {
     }
 
     condition {
-      test = "StringEquals"
+      test   = "StringEquals"
       values = [
         tostring(data.aws_caller_identity.current.account_id)
       ]
